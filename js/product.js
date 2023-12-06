dab16a33332343b8bd4bb0c489ecf6997ef54114
abubakir88 <abubakirsaidxonov88@gmail.com>
2023-12-07 01:20:58 +0500

    AAAAAAAAAAAAAAAAAAAAAG'

---
+++ js/product.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var element = document.getElementById("myDIV");
+  element.classList.toggle("mystyle");
+}
+
+const btnOpen = document.getElementById("btn-open");
+const btnClose = document.getElementById("btn-close");
+const modal = document.querySelector(".modal");
+
+btnOpen.addEventListener("click", function (e) {
+  modal.classList.add("open");
+});
+
+btnClose.addEventListener("click", function (e) {
+  modal.classList.remove("open");
+});
+s;
